97765c3f55dce9dfec02432b4a0bd3a87b3fea0b
hari-vishwanath <mailtohari134@gmail.com>
2022-12-21 21:38:03 +0530

    started abstraction

--- main.py
+++ main.py
@@ -9,105 +9,189 @@ st.title("PLAYSTORE APP DATA ANALYSIS")
 # creating the dataframe for analysis
 @st.cache
 def create_dataset():
-    data = pd.read_csv('playstore_data.csv')
-    data.columns = ['app_name', 'category', 'rating',
-                    'rating_count', 'maximum_installs',
-                    'free', 'price', 'currency',
-                    'size', 'released', 'content_rating']
+    data = pd.read_csv("playstore_data.csv")
+    data.columns = [
+        "app_name",
+        "category",
+        "rating",
+        "rating_count",
+        "maximum_installs",
+        "free",
+        "price",
+        "currency",
+        "size",
+        "released",
+        "content_rating",
+    ]
     return data
 
 
 # loading dataframe
 df = create_dataset()
 
-# showing options to users for analysis
+# showing categories to users for analysis
 
-options = st.multiselect(
+categories = st.multiselect(
     "1. SELECT CATEGORY OF APPS",
-    ["All", "Board", "Finance", "Role Playing", "Health & Fitness",
-     "Productivity", "Comics", "Adventure", "Puzzle", "Strategy",
-     "Shopping", "Casual", "Simulation", "Education", "Beauty",
-     "Books & Reference", "Arcade", "Auto & Vehicles", "Sports",
-     "Food & Drink", "Dating", "Weather", "Music", "Entertainment",
-     "Communication", "Events", "Medical", "Casino", "Travel & Local",
-     "Maps & Navigation", "Racing", "Action", "Card", "News & Magazines",
-     "Art & Design", "Parenting", "Lifestyle", "Trivia", "Word",
-     "House & Home", "Tools", "Business", "Libraries & Demo", "Social",
-     "Personalization", "Video Players & Editors", "Photography"],
-    ["All"]
+    [
+        "All",
+        "Board",
+        "Finance",
+        "Role Playing",
+        "Health & Fitness",
+        "Productivity",
+        "Comics",
+        "Adventure",
+        "Puzzle",
+        "Strategy",
+        "Shopping",
+        "Casual",
+        "Simulation",
+        "Education",
+        "Beauty",
+        "Books & Reference",
+        "Arcade",
+        "Auto & Vehicles",
+        "Sports",
+        "Food & Drink",
+        "Dating",
+        "Weather",
+        "Music",
+        "Entertainment",
+        "Communication",
+        "Events",
+        "Medical",
+        "Casino",
+        "Travel & Local",
+        "Maps & Navigation",
+        "Racing",
+        "Action",
+        "Card",
+        "News & Magazines",
+        "Art & Design",
+        "Parenting",
+        "Lifestyle",
+        "Trivia",
+        "Word",
+        "House & Home",
+        "Tools",
+        "Business",
+        "Libraries & Demo",
+        "Social",
+        "Personalization",
+        "Video Players & Editors",
+        "Photography",
+    ],
+    ["All"],
 )
 content_rating = st.multiselect(
     "2. SELECT CONTENT RATING OF APPS",
-    ["Unrated", "Everyone 10+", "Teen",
-     "Everyone", "Adults only 18+", "Mature 17+"],
-    ["Everyone"]
+    ["Unrated", "Everyone 10+", "Teen", "Everyone", "Adults only 18+", "Mature 17+"],
+    ["Everyone"],
 )
-rating = st.slider('3. SELECT RATINGS RANGE', 0.0, 5.0, (0.0, 5.0))
+rating = st.slider("3. SELECT RATINGS RANGE", 0.0, 5.0, (0.0, 5.0))
 
-num_of_rating = st.slider("4. SELECT NUMBER OF RATINGS RANGE", 0, 56025424, (0, 56025424))
+num_of_rating = st.slider(
+    "4. SELECT NUMBER OF RATINGS RANGE", 0, 56025424, (0, 56025424)
+)
 
-cost = st.radio(
-    "5. APP SHOULD BE(FREE OR PAID)?",
-    ('ANY', 'FREE', 'PAID'))
+cost = st.radio("5. APP SHOULD BE(FREE OR PAID)?", ("ANY", "FREE", "PAID"))
 
-app_size = st.slider("6. SELECT RANGE OF SIZE OF THE APP(in MB)", 1, 999, (1, 999))
+app_size = st.slider(
+    "6. SELECT RANGE OF SIZE OF THE APP(in MB)", 1, 999, (1, 999))
 
 
 # input interpretation
 
 # cost of app
-if cost == 'FREE':
+if cost == "FREE":
     query_cost = 1
-elif cost == 'PAID':
+elif cost == "PAID":
     query_cost = 0
 else:
     query_cost = 2
 
-# categories of app
-opts = []
-for i in range(0, len(options)):
-    opts.append(options[i])
-
-# content rating of apps
-content_rating_opts = []
-for i in range(0, len(content_rating)):
-    content_rating_opts.append(content_rating[i])
 
 
 if query_cost == 1:
-    if 'All' in opts:
-        df_category = df[~df['category'].isin([]) & df['content_rating'].isin(content_rating_opts) & (
-            (df['rating'] > rating[0]) & (df['rating'] < rating[1])) &
-            ((df['rating_count'] > num_of_rating[0]) & (df['rating_count'] < num_of_rating[1]) & df['free'] == 1) &
-            ((df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+    if "All" in categories:
+        df_category = df[
+            ~df["category"].isin([])
+            & df["content_rating"].isin(content_rating)
+            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+                & df["free"]
+                == 1
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
     else:
-        df_category = df[df['category'].isin(opts) & df['content_rating'].isin(content_rating_opts) & (
-            (df['rating'] > rating[0]) & (df['rating'] < rating[1])) &
-            ((df['rating_count'] > num_of_rating[0]) & (df['rating_count'] < num_of_rating[1]) & df['free'] == 1) &
-            ((df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+        df_category = df[
+            df["category"].isin(categories)
+            & df["content_rating"].isin(content_rating)
+            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+                & df["free"]
+                == 1
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
 
 elif query_cost == 0:
-    if 'All' in opts:
-        df_final = df[~df['category'].isin([]) & df['content_rating'].isin(content_rating_opts) & (
-            (df['rating'] > rating[0]) & (df['rating'] < rating[1])) &
-            ((df['rating_count'] > num_of_rating[0]) & (df['rating_count'] < num_of_rating[1]) & df['free'] == 0) &
-            ((df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+    if "All" in categories:
+        df_final = df[
+            ~df["category"].isin([])
+            & df["content_rating"].isin(content_rating)
+            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+                & df["free"]
+                == 0
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
     else:
-        df_final = df[df['category'].isin(opts) & df['content_rating'].isin(content_rating_opts) & (
-            df['rating'] > rating[0]) & (df['rating'] < rating[1]) &((df['rating_count'] > num_of_rating[0]) &
-            (df['rating_count'] < num_of_rating[1]) & df['free'] == 0) &
-            ((df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+        df_final = df[
+            df["category"].isin(categories)
+            & df["content_rating"].isin(content_rating)
+            & (df["rating"] > rating[0])
+            & (df["rating"] < rating[1])
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+                & df["free"]
+                == 0
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
 else:
-    if 'All' in opts:
-        df_final = df[~df['category'].isin([]) & df['content_rating'].isin(content_rating_opts) & (
-            (df['rating'] > rating[0]) & (df['rating'] < rating[1])) &
-            ((df['rating_count'] > num_of_rating[0]) & (df['rating_count'] < num_of_rating[1])) &
-            ((df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+    if "All" in categories:
+        df_final = df[
+            ~df["category"].isin([])
+            & df["content_rating"].isin(content_rating)
+            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
     else:
-        df_final = df[df['category'].isin(opts) & df['content_rating'].isin(content_rating_opts) & (
-            (df['rating'] > rating[0]) & (df['rating'] < rating[1])) &
-            ((df['rating_count'] > num_of_rating[0]) & (df['rating_count'] < num_of_rating[1])) & (
-            (df['size'] > app_size[0]) & (df['size'] < app_size[1]))]
+        df_final = df[
+            df["category"].isin(categories)
+            & df["content_rating"].isin(content_rating)
+            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+            & (
+                (df["rating_count"] > num_of_rating[0])
+                & (df["rating_count"] < num_of_rating[1])
+            )
+            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+        ]
 # df_rating = df_category[(df_category['rating'] > rating[0]) & (df_category['rating'] < rating[1])]
 # df_num_ratings = df_rating[
 #     (df_rating['rating_count'] > num_of_rating[0]) & (df_rating['rating_count'] < num_of_rating[1])]
@@ -116,5 +200,5 @@ else:
 st.success("FILTERED RESULT")
 st.write(df_final)
 
-visualization.plot_bar_categories(opts,df_final)
-visualization.plot_bar_content_rating(content_rating_opts,df_final)
+visualization.plot_bar_categories(categories, df_final)
+visualization.plot_bar_content_rating(content_rating, df_final)
--- visualization.py
+++ visualization.py
@@ -1,16 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+from typing import Tuple,List,Dict
+
+
+class visualizer:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def parse_input(**parameters):
+        categories: Tuple = parameters["categories"]
+        content_rating: Tuple = parameters["content_rating"]
+        rating: Tuple= parameters["rating"]
+        num_of_rating: Tuple = parameters["num_of_rating"]
+        cost: str = parameters["cost"]
+        app_size: tuple[str] = parameters["app_size"]
+
+        query_cost = 1 if cost == 'FREE' else 2 if cost == "PAID" else 2
+        parsed_input = {'categories':list(categories),
+                        }
+
+    
 
 
 def plot_bar_categories(categories, df):
     categories_list = []
     count_list = []
     for category in categories:
-        count_list.append(df.loc[df.category == category, 'category'].count())
+        count_list.append(df.loc[df.category == category, "category"].count())
         categories_list.append(category)
     fig = plt.figure(figsize=(20, 10))
-    plt.bar(categories_list, count_list, color='maroon', width=0.4)
+    plt.bar(categories_list, count_list, color="maroon", width=0.4)
     plt.xlabel("Category of app ")
     plt.ylabel("Number of occurrences ")
     plt.title("Category wise bar graph of number of occurrences")
@@ -22,10 +43,13 @@ def plot_bar_content_rating(content_ratings, df):
     content_rating_list = []
     count_list = []
     for content_rating in content_ratings:
-        count_list.append(df.loc[df.content_rating == content_rating, 'content_rating'].count())
+        count_list.append(
+            df.loc[df.content_rating == content_rating,
+                   "content_rating"].count()
+        )
         content_rating_list.append(content_rating)
     fig = plt.figure(figsize=(20, 10))
-    plt.bar(content_rating_list, count_list, color='blue', width=0.4)
+    plt.bar(content_rating_list, count_list, color="blue", width=0.4)
     plt.xlabel("Content rating of app ")
     plt.ylabel("Number of occurrences ")
     plt.title("Category wise bar graph of number of occurrences")
