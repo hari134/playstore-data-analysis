82b6bac8bb97345fd7dd9ead869735a2f3f6c162
hari-vishwanath <mailtohari134@gmail.com>
2022-12-22 22:43:52 +0530

    final app

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import datetime
 import visualization
 
@@ -24,10 +25,9 @@ with st.sidebar:
     st.header("FILTERS")
 
     categories = st.multiselect(
-    "1. SELECT CATEGORY OF APPS",visualization.utility_variables['all_categories'],["All"],)
+        "1. SELECT CATEGORY OF APPS", visualization.utility_variables['all_categories'], ["All"])
 
-    content_rating = st.multiselect(
-        "2. SELECT CONTENT RATING OF APPS",visualization.utility_variables['content_ratings'],["Everyone"])
+    content_rating = st.multiselect("2. SELECT CONTENT RATING OF APPS", visualization.utility_variables['content_ratings'], ["Everyone"])
 
     rating = st.slider("3. SELECT RATINGS RANGE", 0.0, 5.0, (0.0, 5.0))
 
@@ -42,118 +42,128 @@ with st.sidebar:
 
     min_installs_col, max_installs_col = st.columns(2)
     with min_installs_col:
-        min_installs = st.number_input("7. MINIMUM INSTALLS",0,1704495994,0)
+        min_installs = st.number_input("7. MINIMUM INSTALLS", 0, 1704495994, 0)
 
     with max_installs_col:
-        max_installs = st.number_input("MAXIMUM INSTALLS",0,1704495994,1704495994)
-
+        max_installs = st.number_input(
+            "MAXIMUM INSTALLS", 0, 1704495994, 1704495994)
 
     min_date_col, max_date_col = st.columns(2)
     with min_date_col:
-        min_date = st.date_input("8. ENTER MINIMUM RELEASE DATE ",datetime.date(2010,1,28))
+        min_date = st.date_input(
+            "8. ENTER MINIMUM RELEASE DATE ", datetime.date(2010, 1, 28))
 
     with max_date_col:
-        max_date = st.date_input("  ENTER MAXIMUM RELEASE DATE ",datetime.date(2021,6,15))
+        max_date = st.date_input(
+            "  ENTER MAXIMUM RELEASE DATE ", datetime.date(2021, 6, 15))
     # input interpretation
 
+filter_input = {"categories": categories, "cost": cost, "content_rating": content_rating, "rating": rating,
+                "num_of_rating": num_of_rating, "app_size": app_size, "installs": {"min_installs": min_installs, "max_installs": max_installs},
+                "released":{"min_date":np.datetime64(min_date),"max_date":np.datetime64(max_date)}}
+
+v = visualization.visualizer(df,**filter_input)
+filtered_df = v.get_filtered_data()
+v.plot_bar_categories()
+v.plot_bar_content_rating()
+
+st.success("FILTERED RESULT")
+st.write(filtered_df)
+
+# visualization.plot_bar_categories(categories, df_final)
+# visualization.plot_bar_content_rating(content_rating, df_final)
 # cost of app
-if cost == "FREE":
-    query_cost = 1
-elif cost == "PAID":
-    query_cost = 0
-else:
-    query_cost = 2
+# if cost == "FREE":
+#     query_cost = 1
+# elif cost == "PAID":
+#     query_cost = 0
+# else:
+#     query_cost = 2
 
 
+# if query_cost == 1:
+#     if "All" in categories:
+#         df_category = df[
+#             ~df["category"].isin([])
+#             & df["content_rating"].isin(content_rating)
+#             & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#                 & df["free"]
+#                 == 1
+#             )
+#             & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+#         ]
+#     else:
+#         df_category = df[
+#             df["category"].isin(categories)
+#             & df["content_rating"].isin(content_rating)
+#             & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#                 & df["free"]
+#                 == 1
+#             )
+#             & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+#         ]
 
-if query_cost == 1:
-    if "All" in categories:
-        df_category = df[
-            ~df["category"].isin([])
-            & df["content_rating"].isin(content_rating)
-            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-                & df["free"]
-                == 1
-            )
-            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
-        ]
-    else:
-        df_category = df[
-            df["category"].isin(categories)
-            & df["content_rating"].isin(content_rating)
-            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-                & df["free"]
-                == 1
-            )
-            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
-        ]
-
-elif query_cost == 0:
-    if "All" in categories:
-        df_final = df[
-            ~df["category"].isin([])
-            & df["content_rating"].isin(content_rating)
-            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-                & df["free"]
-                == 0
-            )
-            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
-        ]
-    else:
-        df_final = df[
-            df["category"].isin(categories)
-            & df["content_rating"].isin(content_rating)
-            & (df["rating"] > rating[0])
-            & (df["rating"] < rating[1])
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-                & df["free"]
-                == 0
-            )
-            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
-        ]
-else:
-    if "All" in categories:
-        df_final = df[
-            ~df["category"].isin([])
-            & df["content_rating"].isin(content_rating)
-            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-            )
-            & (df["size"] > app_size[0]) & (df["size"] < app_size[1])
-            & ((df['maximum_installs'] < max_installs) & (df['maximum_installs'] > min_installs))
-            & ((df['released'] < '2021-06-15') & (df['released'] > '2010-01-28'))
-        ]
-    else:
-        df_final = df[
-            df["category"].isin(categories)
-            & df["content_rating"].isin(content_rating)
-            & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
-            & (
-                (df["rating_count"] > num_of_rating[0])
-                & (df["rating_count"] < num_of_rating[1])
-            )
-            & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
-        ]
+# elif query_cost == 0:
+#     if "All" in categories:
+#         df_final = df[
+#             ~df["category"].isin([])
+#             & df["content_rating"].isin(content_rating)
+#             & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#                 & df["free"]
+#                 == 0
+#             )
+#             & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+#         ]
+#     else:
+#         df_final = df[
+#             df["category"].isin(categories)
+#             & df["content_rating"].isin(content_rating)
+#             & (df["rating"] > rating[0])
+#             & (df["rating"] < rating[1])
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#                 & df["free"]
+#                 == 0
+#             )
+#             & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+#         ]
+# else:
+#     if "All" in categories:
+#         df_final = df[
+#             ~df["category"].isin([])
+#             & df["content_rating"].isin(content_rating)
+#             & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#             )
+#             & (df["size"] > app_size[0]) & (df["size"] < app_size[1])
+#             & ((df['maximum_installs'] < max_installs) & (df['maximum_installs'] > min_installs))
+#             & ((df['released'] < '2021-06-15') & (df['released'] > '2010-01-28'))
+#         ]
+#     else:
+#         df_final = df[
+#             df["category"].isin(categories)
+#             & df["content_rating"].isin(content_rating)
+#             & ((df["rating"] > rating[0]) & (df["rating"] < rating[1]))
+#             & (
+#                 (df["rating_count"] > num_of_rating[0])
+#                 & (df["rating_count"] < num_of_rating[1])
+#             )
+#             & ((df["size"] > app_size[0]) & (df["size"] < app_size[1]))
+#         ]
 # df_rating = df_category[(df_category['rating'] > rating[0]) & (df_category['rating'] < rating[1])]
 # df_num_ratings = df_rating[
 #     (df_rating['rating_count'] > num_of_rating[0]) & (df_rating['rating_count'] < num_of_rating[1])]
 # df_final = df_num_ratings
 # print(df_final)
-st.success("FILTERED RESULT")
-st.write(df_final)
-
-visualization.plot_bar_categories(categories, df_final)
-visualization.plot_bar_content_rating(content_rating, df_final)
--- visualization.py
+++ visualization.py
@@ -68,11 +68,10 @@ all_categories: list[str] = [
     "Photography",
 ]
 
-content_ratings: list[str] = (
-    ["Unrated", "Everyone 10+", "Teen", "Everyone", "Adults only 18+", "Mature 17+"],
-)
+content_ratings: list[str] = ["Unrated", "Everyone 10+", "Teen", "Everyone", "Adults only 18+", "Mature 17+"]
 
-utility_variables: dict[str,list[str]] = {
+
+utility_variables: dict[str, list[str]] = {
     "all_categories": all_categories,
     "content_ratings": content_ratings,
     "column_names": column_names,
@@ -80,16 +79,17 @@ utility_variables: dict[str,list[str]] = {
 
 
 class visualizer:
-    def __init__(self,dataframe, **kwargs):
+    def __init__(self, dataframe, **kwargs):
         self.parameters = kwargs
         self.df = dataframe
 
-    
-    def parse_input(self) -> dict[str,any]:
-        categories: list[str] = self.parameters["categories"]
-        cost: str = self.parameters["cost"]
+    @staticmethod
+    def parse_input(parameters) -> dict[str, any]:
+        categories: list[str] = parameters["categories"]
+        cost: str = parameters["cost"]
 
-        query_cost = [1] if cost == "FREE" else [0] if cost == "PAID" else [1, 2]
+        query_cost = [1] if cost == "FREE" else [
+            0] if cost == "PAID" else [1, 0]
         categories = (
             categories
             if "All" not in categories
@@ -98,60 +98,64 @@ class visualizer:
         parsed_input = {
             "categories": categories,
             "cost": query_cost,
-            "content_rating" :self.parameters["content_rating"],
-            "rating": self.parameters["rating"],
-            "num_of_rating": self.parameters["num_of_rating"],
-            "app_size": self.parameters["app_size"],
-            "installs":self.parameters["installs"],
-            "released":self.parameters['released']
+            "content_rating": parameters["content_rating"],
+            "rating": parameters["rating"],
+            "num_of_rating": parameters["num_of_rating"],
+            "app_size": parameters["app_size"],
+            "installs": parameters["installs"],
+            "released": parameters['released']
         }
         return parsed_input
-    
+
     def get_filtered_data(self) -> pd.DataFrame:
         df = self.df
-        parsed_input = visualizer.parse_input()
+        parsed_input = visualizer.parse_input(self.parameters)
         df_filtered = df[
             df["category"].isin(parsed_input['categories'])
+            & (df['free'].isin(parsed_input['cost']))
             & df["content_rating"].isin(parsed_input['content_rating'])
-            & ((df["rating"] > parsed_input['rating'][0]) & (parsed_input['rating'][1]))
+            & ((df["rating"] > parsed_input['rating'][0]) & (df["rating"]<parsed_input['rating'][1]))
             & (
                 (df["rating_count"] > parsed_input['num_of_rating'][0])
                 & (df["rating_count"] < parsed_input['num_of_rating'][1])
             )
             & (df["size"] > parsed_input['app_size'][0]) & (df["size"] < parsed_input['app_size'][1])
-            & ((df['maximum_installs'] < parsed_input['installs']['max_installs']) & (df['installs'] > parsed_input['installs']['min_installs']))
+            & ((df['maximum_installs'] < parsed_input['installs']['max_installs']) & (df['maximum_installs'] > parsed_input['installs']['min_installs']))
             & ((df['released'] < parsed_input['released']['max_date']) & (df['released'] > parsed_input['released']['min_date']))
         ]
+        self.fileterd_df = df_filtered
         return df_filtered
 
+    def plot_bar_categories(self):
+        df = self.fileterd_df
+        categories_list = []
+        count_list = []
+        for category in self.parameters["categories"]:
+            count_list.append(
+                df.loc[df.category == category, "category"].count())
+            categories_list.append(category)
+        fig = plt.figure(figsize=(20, 10))
+        plt.bar(categories_list, count_list, color="maroon", width=0.4)
+        plt.xlabel("Category of app ")
+        plt.ylabel("Number of occurrences ")
+        plt.title("Category wise bar graph of number of occurrences")
+        st.success("APP CATEGORY WISE BAR GRAPH")
+        st.pyplot(fig)
 
-def plot_bar_categories(categories, df):
-    categories_list = []
-    count_list = []
-    for category in categories:
-        count_list.append(df.loc[df.category == category, "category"].count())
-        categories_list.append(category)
-    fig = plt.figure(figsize=(20, 10))
-    plt.bar(categories_list, count_list, color="maroon", width=0.4)
-    plt.xlabel("Category of app ")
-    plt.ylabel("Number of occurrences ")
-    plt.title("Category wise bar graph of number of occurrences")
-    st.success("APP CATEGORY WISE BAR GRAPH")
-    st.pyplot(fig)
-
-
-def plot_bar_content_rating(content_ratings, df):
-    content_rating_list = []
-    count_list = []
-    for content_rating in content_ratings:
-        count_list.append(
-            df.loc[df.content_rating == content_rating, "content_rating"].count()
-        )
-        content_rating_list.append(content_rating)
-    fig = plt.figure(figsize=(20, 10))
-    plt.bar(content_rating_list, count_list, color="blue", width=0.4)
-    plt.xlabel("Content rating of app ")
-    plt.ylabel("Number of occurrences ")
-    plt.title("Category wise bar graph of number of occurrences")
-    st.success("CONTENT RATING WISE BAR GRAPH")
-    st.pyplot(fig)
+    def plot_bar_content_rating(self):
+        df = self.fileterd_df
+        content_rating_list = []
+        count_list = []
+        for content_rating in self.parameters['content_rating']:
+            count_list.append(
+                df.loc[df.content_rating == content_rating,
+                       "content_rating"].count()
+            )
+            content_rating_list.append(content_rating)
+        fig = plt.figure(figsize=(20, 10))
+        plt.bar(content_rating_list, count_list, color="blue", width=0.4)
+        plt.xlabel("Content rating of app ")
+        plt.ylabel("Number of occurrences ")
+        plt.title("Category wise bar graph of number of occurrences")
+        st.success("CONTENT RATING WISE BAR GRAPH")
+        st.pyplot(fig)
